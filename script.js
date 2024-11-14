4f5cb47f6d6566f2e73a038c24665add55b48285
irodakarimov <irodakarimova896@gmail.com>
2024-11-14 10:28:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a = 5;
+let b = 853290;
+const c = 87;
+
+let userName = prompt("String typedagi malumot kiriting");
+let surName = prompt("Number typedagi malumot kiriting");
+let ageName = prompt("Boolean typedagi malumot kiriting");
+alert(`Hamma malumotlarni to'g'ri kiritganizga ishonchingiz komilmi?`);
+alert(`Siz kiritgan Muhammad ayyub so'zi string typega tegishli
+Siz kiritgan 123 soni nummber typega tegishli
+Siz kiritgan true so'zi boolean typega tegishli
+`)
